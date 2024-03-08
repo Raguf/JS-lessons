3b17f9fb47e376832803978241b2b875a25f15dd
Raguf <97394996+Raguf@users.noreply.github.com>
2024-03-08 17:25:37 +0400

    let, const and var

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ console.log(`My name is ${fullName}, I'm ${age} old, and I'm ${job}.`);*/
 
 /* ---------- Data Types --------- */
 
-let fullName = "Ramey"; // string
+/*let fullName = "Ramey"; // string
 let age = 34; // number
 let rameyIsDeveloper = true; // boolean
 let rameyCar; // undefined
@@ -16,4 +16,14 @@ let rameyCar; // undefined
 console.log(fullName, typeof fullName);
 console.log(age, typeof age);
 console.log(rameyIsDeveloper, typeof rameyIsDeveloper);
-console.log(rameyCar, typeof rameyCar);
+console.log(rameyCar, typeof rameyCar);*/
+
+/* ---------- let, const and var --------- */
+
+let age = 30; // allow to change
+age = 32;
+
+const birthYear = 1989; // don't allow to change
+
+var job = "programmer"; // like to "let", but "var"
+job = "driver";
