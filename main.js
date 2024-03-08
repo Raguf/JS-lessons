a73bffc1abd775a5bae482ca3083a2ca9d3dfb4e
Raguf <97394996+Raguf@users.noreply.github.com>
2024-03-08 17:05:17 +0400

    Data Types

--- main.js
+++ main.js
@@ -1,6 +1,19 @@
-// ---------- Values and variables ---------
-let fullName = "Ramey";
+/* ---------- Values and variables ---------*/
+
+/*let fullName = "Ramey";
 let age = 34;
 let job = "developer";
 
-console.log(`My name is ${fullName}, I'm ${age} old, and I'm ${job}.`);
+console.log(`My name is ${fullName}, I'm ${age} old, and I'm ${job}.`);*/
+
+/* ---------- Data Types --------- */
+
+let fullName = "Ramey"; // string
+let age = 34; // number
+let rameyIsDeveloper = true; // boolean
+let rameyCar; // undefined
+
+console.log(fullName, typeof fullName);
+console.log(age, typeof age);
+console.log(rameyIsDeveloper, typeof rameyIsDeveloper);
+console.log(rameyCar, typeof rameyCar);
