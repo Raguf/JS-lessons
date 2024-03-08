1c311221f7cdb14926936411f642512997538267
Raguf <97394996+Raguf@users.noreply.github.com>
2024-03-08 19:33:28 +0400

    Coding Task #1

--- main.js
+++ main.js
@@ -61,7 +61,7 @@ console.log(ageYusif >= 18);*/
 
 /* ---------- Operator Precedence ---------- */
 
-const nowYear = 2024;
+/*const nowYear = 2024;
 const ageRamey = nowYear - 1989;
 const ageYusif = nowYear - 2001;
 
@@ -74,4 +74,44 @@ console.log(x, y);
 
 const averageAge = (ageRamey + ageYusif) / 2;
 
-console.log(ageRamey, ageYusif, averageAge);
+console.log(ageRamey, ageYusif, averageAge);*/
+
+/* ---------- Coding Task #1 ---------- */
+
+/*
+Instructions:
+1) The Complete JavaScript Course contains over 25 coding challenges for you to practice the concepts we learn in each section, on your own. This is a fundamental part of your learning journey.
+
+2) This document contains all coding challenges in this course, so that you can read the challenge text without watching the video (I still recommend watching the videos because I might give some additional clues)
+
+3) Some challenges require some starting code, which you can get from this document instead of copying it from the videos
+
+4) The solution of each challenge is in the videos themselves, so you still have to go watch them 
+
+5) Some of the later challenges are quite difficult on purpose (after all, they are called challenges, right? ). If one of them is too difficult, don’t worry, this is completely normal! Just start watching the solution, then pause the video, and keep working on your own. Struggling is part of the journey, but you can do it.
+
+
+Coding Challenge #1
+Mark and John are trying to compare their BMI (Body Mass Index), which is calculated using the formula: BMI = mass / height ** 2 = mass / (height * height) (mass in kg and height in meter).
+
+Your tasks:
+1. Store Mark's and John's mass and height in variables
+2. Calculate both their BMIs using the formula (you can even implement both versions)
+3. Create a Boolean variable 'markHigherBMI' containing information about whether Mark has a higher BMI than John.
+
+Test data:
+Data 1: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95 m tall.
+Data 2: Marks weights 95 kg and is 1.88 m tall. John weights 85 kg and is 1.76 m tall.
+*/
+
+const massMark = 78;
+const heightMark = 1.69;
+const massJohn = 92;
+const heightJohn = 1.95;
+
+const BMIMark = massMark / heightMark ** 2;
+const BMIJohn = massJohn / heightJohn ** 2;
+const markHigherBMI = BMIMark > BMIJohn;
+
+console.log(`Mark BMI's is ${BMIMark} & John BMI's is ${BMIJohn}.`);
+console.log(markHigherBMI);
