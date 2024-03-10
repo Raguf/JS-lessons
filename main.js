4956f96d1434efbefd2ea3043e76400da24b9e50
Raguf <97394996+Raguf@users.noreply.github.com>
2024-03-10 19:58:24 +0400

    Coding Task #5

--- main.js
+++ main.js
@@ -547,6 +547,7 @@ console.log(fruitProcessor(2, 4));
 
 /* ---------- Reviewing Functions ---------- */
 
+/*
 const calcAge = function (birthYear) {
   return 2024 - birthYear;
 };
@@ -565,3 +566,43 @@ const yearsUntilRetirement = function (firstName, birthYear) {
 };
 
 console.log(yearsUntilRetirement("Ramey", 0));
+*/
+
+/* ---------- Coding Task #5 ---------- */
+
+/*
+Back to the two gymnastics teams, the Dolphins and the Koalas! There is a new gymnastics discipline, which works differently. Each team competes 3 times, and then the average of the 3 scores is calculated (so one average score per team).
+A team only wins if it has at least double the average score of the other team. Otherwise, no team wins!
+
+Your tasks:
+1. Create an arrow function 'calcAverage' to calculate the average of 3 scores
+2. Use the function to calculate the average for both teams
+3. Create a function 'checkWinner' that takes the average score of each team as parameters ('avgDolhins' and 'avgKoalas'), and then logs the winner to the console, together with the victory points, according to the rule above. Example: "Koalas win (30 vs. 13)"
+4. Use the 'checkWinner' function to determine the winner for both Data 1 and Data 2
+5. Ignore draws this time
+
+Test data:
+Data 1: Dolphins score 44, 23 and 71. Koalas score 65, 54 and 49
+Data 2: Dolphins score 85, 54 and 41. Koalas score 23, 34 and 27
+*/
+
+const calcAverage = (a, b, c) => (a + b + c) / 3;
+
+console.log(calcAverage(3, 4, 5));
+
+const dolphScore = calcAverage(85, 54, 41);
+const koalaScore = calcAverage(23, 34, 27);
+
+console.log(dolphScore, koalaScore);
+
+const checkWinner = function (avgDolph, avgKoala) {
+  if (avgDolph >= 2 * avgKoala) {
+    console.log(`Dolphins wins (${avgDolph} vs. ${avgKoala})`);
+  } else if (avgKoala >= 2 * avgDolph) {
+    console.log(`Koalas wins (${avgKoala} vs. ${avgDolph})`);
+  } else {
+    console.log("No one wins");
+  }
+};
+
+console.log(checkWinner(dolphScore, koalaScore));
