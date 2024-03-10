371fa41dfbdcbbfad3d5613f4e51da42f9aa4825
Raguf <97394996+Raguf@users.noreply.github.com>
2024-03-10 18:16:54 +0400

    Reviewing Functions

--- main.js
+++ main.js
@@ -544,3 +544,24 @@ function fruitProcessor(apples, oranges) {
 
 console.log(fruitProcessor(2, 4));
 */
+
+/* ---------- Reviewing Functions ---------- */
+
+const calcAge = function (birthYear) {
+  return 2024 - birthYear;
+};
+
+const yearsUntilRetirement = function (firstName, birthYear) {
+  const age = calcAge(birthYear);
+  const retirement = 65 - age;
+
+  if (retirement > 0) {
+    console.log(`${firstName} retires in ${retirement} years`);
+    return retirement;
+  } else {
+    console.log(`${firstName} has already retired`);
+    return -1;
+  }
+};
+
+console.log(yearsUntilRetirement("Ramey", 0));
