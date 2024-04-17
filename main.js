9e5f224b2d8e4941d296934d46d2344a0094d7ce
Raguf <97394996+Raguf@users.noreply.github.com>
2024-04-17 11:49:12 +0400

    Rest Pattern and Parameters

--- main.js
+++ main.js
@@ -1544,7 +1544,7 @@ console.log(o, c);
 */
 
 /* ---------- The Spread Operator (...) ---------- */
-
+/*
 'use strict';
 
 const restaurant = {
@@ -1630,3 +1630,91 @@ const newRestaurant = {
   founder: 'Asif Meherremov',
 };
 console.log(newRestaurant);
+*/
+
+/* ---------- Rest Pattern and Parameters ---------- */
+
+'use strict';
+
+const restaurant = {
+  name: 'Gavali',
+  location: 'Asif Meherremov 23, Baku, Azerbaijan',
+  categories: ['National', 'Vegetarian', 'Organic'],
+  startMenu: ['Kete', 'Dovqa', 'Coban salati'],
+  mainMenu: ['Dolma', 'Ash', 'Kebab'],
+
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0,
+      close: 24,
+    },
+  },
+
+  order: function (starterIndex, mainIndex) {
+    return [this.startMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
+  orderDelivery: function ({
+    starterIndex = 1,
+    mainIndex = 0,
+    time = '20:00',
+    adrress,
+  }) {
+    console.log(
+      `Order received! ${this.startMenu[starterIndex]} & ${this.mainMenu[mainIndex]} we will delevery to ${adrress} at ${time}`
+    );
+  },
+
+  orderPasta: function (ing1, ing2, ing3) {
+    console.log(`Here is your pasta with ${ing1},${ing2},${ing3}`);
+  },
+
+  orderPizza: function (mainIngredient, ...otherIngredients) {
+    console.log(mainIngredient);
+    console.log(otherIngredients);
+  },
+};
+
+// 1. Destructuring
+
+// Spread, because on right side of =
+const arr = [1, 2, ...[3, 4, 5]];
+
+// Rest, because on left side of =
+const [a, b, ...others] = [1, 2, 3, 4, 5];
+console.log(a, b, others); // 1,2 [3, 4, 5]
+console.log(a, b, ...others); // 1 2 3 4 5
+
+const [dolma, , kebab, ...otherFood] = [
+  ...restaurant.mainMenu,
+  ...restaurant.startMenu,
+];
+console.log(dolma, kebab, otherFood);
+
+// Objects
+const { sat, ...weekdays } = restaurant.openingHours;
+console.log(weekdays);
+
+// 2. Functions
+const add = function (...numbers) {
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i++) sum += numbers[i];
+  console.log(sum);
+};
+add(2, 3);
+add(5, 3, 7, 2);
+add(8, 2, 5, 3, 2, 1, 4);
+
+const x = [23, 5, 7];
+add(...x);
+
+// Real example
+restaurant.orderPizza('Gobelek', 'Pomidor', 'Pendir', 'File');
